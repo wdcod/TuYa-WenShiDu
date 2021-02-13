3920d52bf624a25599811a7c858a9d6d4df982ca
wdcod <72258682+wdcod@users.noreply.github.com>
2021-02-13 22:00:59 +0800

    Add files via upload

---
+++ TuYaF030K6T6 V2.0/Debug/Core/User_Lib/Src/mcu_api.d
@@ -0,0 +1,147 @@
+Core/User_Lib/Src/mcu_api.o: ../Core/User_Lib/Src/mcu_api.c \
+ ../Core/User_Lib/Inc/wifi.h ../Core/User_Lib/Inc/protocol.h \
+ ../Core/User_Lib/Inc/system.h ../Core/User_Lib/Inc/mcu_api.h \
+ ../Core/Inc/main.h ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ ../Core/Inc/stm32f0xx_hal_conf.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x6.h \
+ ../Drivers/CMSIS/Include/core_cm0.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_exti.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_iwdg.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rtc.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rtc_ex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_crs.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_rcc.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_bus.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_system.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_exti.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_cortex.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_utils.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_pwr.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_dma.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_tim.h \
+ ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_gpio.h \
+ ../Core/User_Lib/Inc/oled.h ../Core/User_Lib/Inc/hard.h
+
+../Core/User_Lib/Inc/wifi.h:
+
+../Core/User_Lib/Inc/protocol.h:
+
+../Core/User_Lib/Inc/system.h:
+
+../Core/User_Lib/Inc/mcu_api.h:
+
+../Core/Inc/main.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+../Core/Inc/stm32f0xx_hal_conf.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x6.h:
+
+../Drivers/CMSIS/Include/core_cm0.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_exti.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_adc_ex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_iwdg.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rtc.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rtc_ex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_crs.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_rcc.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_bus.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_system.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_exti.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_cortex.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_utils.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_pwr.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_dma.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_tim.h:
+
+../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_ll_gpio.h:
+
+../Core/User_Lib/Inc/oled.h:
+
+../Core/User_Lib/Inc/hard.h:
---
+++ TuYaF030K6T6 V2.0/Debug/Core/User_Lib/Src/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/User_Lib/Src/Hard.c \
+../Core/User_Lib/Src/mcu_api.c \
+../Core/User_Lib/Src/oled.c \
+../Core/User_Lib/Src/protocol.c \
+../Core/User_Lib/Src/system.c 
+
+OBJS += \
+./Core/User_Lib/Src/Hard.o \
+./Core/User_Lib/Src/mcu_api.o \
+./Core/User_Lib/Src/oled.o \
+./Core/User_Lib/Src/protocol.o \
+./Core/User_Lib/Src/system.o 
+
+C_DEPS += \
+./Core/User_Lib/Src/Hard.d \
+./Core/User_Lib/Src/mcu_api.d \
+./Core/User_Lib/Src/oled.d \
+./Core/User_Lib/Src/protocol.d \
+./Core/User_Lib/Src/system.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/User_Lib/Src/Hard.o: ../Core/User_Lib/Src/Hard.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F030x6 -DUSE_FULL_LL_DRIVER -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I../Core/User_Lib/Inc -I../Core/User_Lib/Src -O1 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/User_Lib/Src/Hard.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Core/User_Lib/Src/mcu_api.o: ../Core/User_Lib/Src/mcu_api.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F030x6 -DUSE_FULL_LL_DRIVER -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I../Core/User_Lib/Inc -I../Core/User_Lib/Src -O1 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/User_Lib/Src/mcu_api.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Core/User_Lib/Src/oled.o: ../Core/User_Lib/Src/oled.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F030x6 -DUSE_FULL_LL_DRIVER -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I../Core/User_Lib/Inc -I../Core/User_Lib/Src -O1 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/User_Lib/Src/oled.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Core/User_Lib/Src/protocol.o: ../Core/User_Lib/Src/protocol.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F030x6 -DUSE_FULL_LL_DRIVER -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I../Core/User_Lib/Inc -I../Core/User_Lib/Src -O1 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/User_Lib/Src/protocol.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Core/User_Lib/Src/system.o: ../Core/User_Lib/Src/system.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F030x6 -DUSE_FULL_LL_DRIVER -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I../Core/User_Lib/Inc -I../Core/User_Lib/Src -O1 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/User_Lib/Src/system.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
